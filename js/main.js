48ebeb3ee43d7de691770d81fc6ee047822c4e54
Meihtor <hils.dev15@gmail.com>
2024-01-02 14:18:43 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	$('.flexslider').flexslider({
+		prevText: "",
+		nextText: "",
+		pauseOnAction: false,
+		slideshowSpeed: 4000,
+		touch: true
+
+
+
+	});
+
+
+});
